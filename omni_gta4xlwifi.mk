c76b07fd34fe74688d261793b45b148adc8b03a7
Huina1aosp <huinadiscordforhuina@gmail.com>
2024-08-06 00:11:50 +0900

    d

--- omni_gta4xlwifi.mk
+++ omni_gta4xlwifi.mk
@@ -5,22 +5,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := gta4xlwifi
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from gta4xlwifi device
 $(call inherit-product, device/samsung/gta4xlwifi/device.mk)
 
-## device identifier
+#device indentifier
 PRODUCT_DEVICE := gta4xlwifi
-PRODUCT_NAME := twrp_gta4xlwifi
+PRODUCT_NAME := omni_gta4xlwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-P610
 PRODUCT_MANUFACTURER := samsung
